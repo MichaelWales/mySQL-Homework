ebd179cc49b3e9bde689c1885d54065a9cefd741
Michael Wales <michaelwales@gmail.com>
2020-03-23 19:05:21 +0000

    Adds answer for Q3

--- marvel.sql
+++ marvel.sql
@@ -50,4 +50,5 @@ INSERT INTO movies (title, year, show_time) VALUES ('Spider-Man: Homecoming', 20
 INSERT INTO movies (title, year, show_time) VALUES ('Thor: Ragnarok', 2017, '22:10');
 INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:00');
 
-SELECT (title) FROM movies;
+UPDATE people SET name = 'Krusty the Clown' WHERE name = 'Crusty the Clown';
+SELECT (name) FROM people;
